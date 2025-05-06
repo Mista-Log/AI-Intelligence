d2cb838c5ddbed30bf6e2170a9a3d80dbef8828f
Ibrahim Oloyede <159485173+Mista-Log@users.noreply.github.com>
2025-05-06 23:55:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
   </React.StrictMode>,
 )
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+)
